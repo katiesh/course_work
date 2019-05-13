449d5a06132316915143ffc50311f496a0235a04
curl http://installrails.com/update_rubygems.rb | ruby <eshkulyova@gmail.com>
2019-05-13 18:26:55 +0300

    Update Service.java
    add and implement methods for calculation of initial and boundary conditions. init constants.

--- src/com/company/Service.java
+++ src/com/company/Service.java
@@ -1,5 +1,22 @@
 package com.company;
 
 public class Service {
-  
+    public static final double a = 0;
+    public static final double b = 0;
+    public static final double A = 0;
+    public static final double B = 0;
+
+    public double calculateInitialConditions(double x){
+        return calculateExactValue(x,0);
+    }
+    public double calculateBoundaryConditionsForOne(double t){
+        return calculateExactValue(1, t);
+    }
+    public double calculateBoundaryConditionsForZero(double t){
+        return calculateExactValue(0, t);
+    }
+
+    public double calculateExactValue(double x, double t){
+        return Math.exp(b*t)*Math.pow((A*x + B), 0.5D);
+    }
 }
