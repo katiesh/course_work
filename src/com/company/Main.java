7c9d25397d514ee790e5fef3144340f27db0f3f7
curl http://installrails.com/update_rubygems.rb | ruby <eshkulyova@gmail.com>
2019-05-13 21:28:21 +0300

    add calculation of exact value and fix sequantial calculation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,25 +8,35 @@ public class Main {
         double stepT;
         int numberOfStepsX = 10;
         int numberOfStepsT = 100;
+        double[][] exactValue = new double[numberOfStepsX][numberOfStepsT];
         double[][] sequantial = new double[numberOfStepsX][numberOfStepsT];
         double x = 0D;
+        double t = 0D;
+        for (int i = 0; i < numberOfStepsX ; i++) {
+            for (int j = 0; j < numberOfStepsT ; j++) {
+                exactValue[i][j] = service.calculateExactValue(x,t);
+                t+=stepT;
+            }
+            x+=stepX;
+        }
+        x = 0D;
         for (int i = 0; i < numberOfStepsX; i++) {
             sequantial[i][0] = service.calculateInitialConditions(x);
             x+=stepX;
         }
-        double t = 0D;
+        t = 0D;
         for (int i = 0; i < numberOfStepsT; i++) {
             sequantial[0][i] = service.calculateBoundaryConditionsForZero(t);
-            sequantial[1][i] = service.calculateBoundaryConditionsForOne(t);
+            sequantial[numberOfStepsX-1][i] = service.calculateBoundaryConditionsForOne(t);
             t+=stepT;
         }
-        x = 0D;
-        t = 0D;
-        for (int i = 1; i < numberOfStepsX; i++) {
-            for (int j = 1; j < numberOfStepsT -1 ; j++) {
+        for (int j = 1; j < numberOfStepsT-1; j++) {
+            for (int i = 1; i < numberOfStepsX; i++) {
                 sequantial[i][j] = service.calculateWgrid(sequantial[i][j-1], sequantial[i-1][j-1],
                         sequantial[i+1][j-1], stepX, stepT);
             }
         }
+
+
     }
 }
