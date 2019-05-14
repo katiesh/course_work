55de941bc79badbfcedc1f21af7da38880be0db6
curl http://installrails.com/update_rubygems.rb | ruby <eshkulyova@gmail.com>
2019-05-14 18:27:20 +0300

    initialize constants and fix calculateWgrid method in Service.java

--- src/com/company/Service.java
+++ src/com/company/Service.java
@@ -1,10 +1,10 @@
 package com.company;
 
 public class Service {
-    public static final double a = 0;
-    public static final double b = 0;
-    public static final double A = 0;
-    public static final double B = 0;
+    public static final double a = 1.7;
+    public static final double b = 0.5;
+    public static final double A = 72;
+    public static final double B = 14;
 
     public double calculateInitialConditions(double x){
         return calculateExactValue(x,0);
@@ -15,11 +15,10 @@ public class Service {
     public double calculateBoundaryConditionsForZero(double t){
         return calculateExactValue(0, t);
     }
-
     public double calculateExactValue(double x, double t){
         return Math.exp(b*t)*Math.pow((A*x + B), 0.5D);
     }
     public double calculateWgrid(double wi, double wiPrev, double wiNext, double h, double tau){
-        return wi + tau*(a*((wiNext - wiPrev)/2*h + wi*((wiPrev - 2*wi + wiNext)/Math.pow(h,2))) + b*wi);
+        return wi + tau*(a*(Math.pow((wiNext - wiPrev)/(2D*h), 2) + wi*((wiPrev - 2D*wi + wiNext)/Math.pow(h,2))) + b*wi);
     }
 }
